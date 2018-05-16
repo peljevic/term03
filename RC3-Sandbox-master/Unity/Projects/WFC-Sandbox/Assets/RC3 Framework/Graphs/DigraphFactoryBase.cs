3bf0e21fceda3d281c615157aba49edc1ac4fbe5
peljevic <j.peljevic.17@ucl.ac.uk>
2018-05-16 20:02:07 +0100

    changes
    changes

--- RC3-Sandbox-master/Unity/Projects/WFC-Sandbox/Assets/RC3 Framework/Graphs/DigraphFactoryBase.cs
+++ RC3-Sandbox-master/Unity/Projects/WFC-Sandbox/Assets/RC3 Framework/Graphs/DigraphFactoryBase.cs
@@ -339,6 +339,13 @@ namespace RC3.Graphs
                 CreateTruncatedOctahedronGridDefault(countX, countY, countZ);
         }
 
+        public G CreateRhombicDodecahedronGrid(int countX, int countY, int countZ, bool uniform = false)
+        {
+            return uniform ?
+                CreateRhombicDodecahedronGridUniform(countX, countY, countZ) :
+                CreateRhombicDodecahedronGridDefault(countX, countY, countZ);
+        }
+
 
         /// <summary>
         /// 
@@ -500,7 +507,6 @@ namespace RC3.Graphs
             return g;
         }
 
-
         /// <summary>
         /// 
         /// </summary>
@@ -542,6 +548,7 @@ namespace RC3.Graphs
                             g.AddEdge(i, i + 1);
                         else
                             g.AddEdge(i, i);
+                       
 
                         // y-1
                         if (y > 0)
@@ -636,6 +643,382 @@ namespace RC3.Graphs
                             g.AddEdge(j, j + 1);
                         else
                             g.AddEdge(j, j);
+                        
+
+                        // y-1
+                        if (y > 0)
+                            g.AddEdge(j, j - countX);
+                        else
+                            g.AddEdge(j, j);
+
+                        // y+1
+                        if (y < lastY)
+                            g.AddEdge(j, j + countX);
+                        else
+                            g.AddEdge(j, j);
+
+                        // z-1
+                        if (z > 0)
+                            g.AddEdge(j, j - countXY);
+                        else
+                            g.AddEdge(j, j);
+
+                        // z+1
+                        if (z < lastZ)
+                            g.AddEdge(j, j + countXY);
+                        else
+                            g.AddEdge(j, j);
+
+                        //
+                        g.AddEdge(j, i);
+
+                        // x+1, y+1, z+1
+                        if (x < lastX && y < lastY && z < lastZ)
+                            g.AddEdge(j, i + countXY + countX + 1);
+                        else
+                            g.AddEdge(j, j);
+
+                        // x+1
+                        if (x < lastX)
+                            g.AddEdge(j, i + 1);
+                        else
+                            g.AddEdge(j, j);
+
+                        // y+1, z+1
+                        if (y < lastY && z < lastZ)
+                            g.AddEdge(j, i + countXY + countX);
+                        else
+                            g.AddEdge(j, j);
+
+                        // y+1
+                        if (y < lastY)
+                            g.AddEdge(j, i + countX);
+                        else
+                            g.AddEdge(j, j);
+
+                        // x+1, z+1
+                        if (x < lastX && z < lastZ)
+                            g.AddEdge(j, i + countXY + 1);
+                        else
+                            g.AddEdge(j, j);
+
+                        // x+1, y+1
+                        if (x < lastX && y < lastY)
+                            g.AddEdge(j, i + countX + 1);
+                        else
+                            g.AddEdge(j, j);
+
+                        // z+1
+                        if (z < lastZ)
+                            g.AddEdge(j, i + countXY);
+                        else
+                            g.AddEdge(j, j);
+                    }
+                }
+            }
+
+            return g;
+        }
+
+
+        private G CreateRhombicDodecahedronGridDefault(int countX, int countY, int countZ)
+        {
+            var g = Create();
+            int countXY = countX * countY;
+            int count = countXY * countZ;
+
+            int lastX = countX - 1;
+            int lastY = countY - 1;
+            int lastZ = countZ - 1;
+
+            // add vertices
+            for (int i = 0; i < count; i++)
+            {
+                g.AddVertex();
+                g.AddVertex();
+            }
+
+            // add even layer edges
+            for (int z = 0; z < countZ; z++)
+            {
+                for (int y = 0; y < countY; y++)
+                {
+                    for (int x = 0; x < countX; x++)
+                    {
+                        int i = x + y * countX + z * countXY;
+                        int j = i + count;
+
+                        /* x-1
+                        if (x > 0)
+                            g.AddEdge(i, i - 1);
+
+                        // x+1
+                        if (x < lastX)
+                            g.AddEdge(i, i + 1);
+                        */
+
+                        // y-1
+                        if (y > 0)
+                            g.AddEdge(i, i - countX);
+
+                        // y+1
+                        if (y < lastY)
+                            g.AddEdge(i, i + countX);
+
+                        // z-1
+                        if (z > 0)
+                            g.AddEdge(i, i - countXY);
+
+                        // z+1
+                        if (z < lastZ)
+                            g.AddEdge(i, i + countXY);
+                        
+
+                        // x-1, y-1, z-1
+                        if (x > 0 && y > 0 && z > 0)
+                            g.AddEdge(i, j - countXY - countX - 1);
+
+                        //
+                        g.AddEdge(i, j);
+
+                        // y-1, z-1
+                        if (y > 0 && z > 0)
+                            g.AddEdge(i, j - countXY - countX);
+
+                        // x-1
+                        if (x > 0)
+                            g.AddEdge(i, j - 1);
+
+                        // x-1, z-1
+                        if (x > 0 && z > 0)
+                            g.AddEdge(i, j - countXY - 1);
+
+                        // y-1
+                        if (y > 0)
+                            g.AddEdge(i, j - countX);
+
+                        // z-1
+                        if (z > 0)
+                            g.AddEdge(i, j - countXY);
+                       
+
+                        // x-1, y-1
+                        if (x > 0 && y > 0)
+                            g.AddEdge(i, j - countX - 1);
+                    }
+                }
+            }
+
+            // add odd layer edges
+            for (int z = 0; z < countZ; z++)
+            {
+                for (int y = 0; y < countY; y++)
+                {
+                    for (int x = 0; x < countX; x++)
+                    {
+                        int i = x + y * countX + z * countXY;
+                        int j = i + count;
+
+                        /* x-1
+                        if (x > 0)
+                            g.AddEdge(j, j - 1);
+
+                        // x+1
+                        if (x < lastX)
+                            g.AddEdge(j, j + 1);
+                        */
+
+                        // y-1
+                        if (y > 0)
+                            g.AddEdge(j, j - countX);
+
+                        // y+1
+                        if (y < lastY)
+                            g.AddEdge(j, j + countX);
+
+                        // z-1
+                        if (z > 0)
+                            g.AddEdge(j, j - countXY);
+
+                        // z+1
+                        if (z < lastZ)
+                            g.AddEdge(j, j + countXY);
+
+                        //
+                        g.AddEdge(j, i);
+
+                        // x+1, y+1, z+1
+                        if (x < lastX && y < lastY && z < lastZ)
+                            g.AddEdge(j, i + countXY + countX + 1);
+
+                        // x+1
+                        if (x < lastX)
+                            g.AddEdge(j, i + 1);
+
+                        // y+1, z+1
+                        if (y < lastY && z < lastZ)
+                            g.AddEdge(j, i + countXY + countX);
+
+                        // y+1
+                        if (y < lastY)
+                            g.AddEdge(j, i + countX);
+
+                        // x+1, z+1
+                        if (x < lastX && z < lastZ)
+                            g.AddEdge(j, i + countXY + 1);
+
+                        // x+1, y+1
+                        if (x < lastX && y < lastY)
+                            g.AddEdge(j, i + countX + 1);
+
+                        // z+1
+                        if (z < lastZ)
+                            g.AddEdge(j, i + countXY);
+                    }
+                }
+            }
+
+            return g;
+        }
+
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        private G CreateRhombicDodecahedronGridUniform(int countX, int countY, int countZ)
+        {
+            var g = Create();
+            int countXY = countX * countY;
+            int count = countXY * countZ;
+
+            int lastX = countX - 1;
+            int lastY = countY - 1;
+            int lastZ = countZ - 1;
+
+            // add vertices
+            for (int i = 0; i < count; i++)
+            {
+                g.AddVertex();
+                g.AddVertex();
+            }
+
+            // add even layer edges
+            for (int z = 0; z < countZ; z++)
+            {
+                for (int y = 0; y < countY; y++)
+                {
+                    for (int x = 0; x < countX; x++)
+                    {
+                        int i = x + y * countX + z * countXY;
+                        int j = i + count;
+
+                        /* x-1
+                        if (x > 0)
+                            g.AddEdge(i, i - 1);
+                        else
+                            g.AddEdge(i, i);
+
+                        // x+1
+                        if (x < lastX)
+                            g.AddEdge(i, i + 1);
+                        else
+                            g.AddEdge(i, i);
+                       */
+
+                        // y-1
+                        if (y > 0)
+                            g.AddEdge(i, i - countX);
+                        else
+                            g.AddEdge(i, i);
+
+                        // y+1
+                        if (y < lastY)
+                            g.AddEdge(i, i + countX);
+                        else
+                            g.AddEdge(i, i);
+
+                        // z-1
+                        if (z > 0)
+                            g.AddEdge(i, i - countXY);
+                        else
+                            g.AddEdge(i, i);
+
+                        // z+1
+                        if (z < lastZ)
+                            g.AddEdge(i, i + countXY);
+                        else
+                            g.AddEdge(i, i);
+
+                        // x-1, y-1, z-1
+                        if (x > 0 && y > 0 && z > 0)
+                            g.AddEdge(i, j - countXY - countX - 1);
+                        else
+                            g.AddEdge(i, i);
+
+                        //
+                        g.AddEdge(i, j);
+
+                        // y-1, z-1
+                        if (y > 0 && z > 0)
+                            g.AddEdge(i, j - countXY - countX);
+                        else
+                            g.AddEdge(i, i);
+
+                        // x-1
+                        if (x > 0)
+                            g.AddEdge(i, j - 1);
+                        else
+                            g.AddEdge(i, i);
+
+                        // x-1, z-1
+                        if (x > 0 && z > 0)
+                            g.AddEdge(i, j - countXY - 1);
+                        else
+                            g.AddEdge(i, i);
+
+                        // y-1
+                        if (y > 0)
+                            g.AddEdge(i, j - countX);
+                        else
+                            g.AddEdge(i, i);
+
+                        // z-1
+                        if (z > 0)
+                            g.AddEdge(i, j - countXY);
+                        else
+                            g.AddEdge(i, i);
+
+                        // x-1, y-1
+                        if (x > 0 && y > 0)
+                            g.AddEdge(i, j - countX - 1);
+                        else
+                            g.AddEdge(i, i);
+                    }
+                }
+            }
+
+            // add odd layer edges
+            for (int z = 0; z < countZ; z++)
+            {
+                for (int y = 0; y < countY; y++)
+                {
+                    for (int x = 0; x < countX; x++)
+                    {
+                        int i = x + y * countX + z * countXY;
+                        int j = i + count;
+
+                        /* x-1
+                        if (x > 0)
+                            g.AddEdge(j, j - 1);
+                        else
+                            g.AddEdge(j, j);
+
+                        // x+1
+                        if (x < lastX)
+                            g.AddEdge(j, j + 1);
+                        else
+                            g.AddEdge(j, j);
+                        */
 
                         // y-1
                         if (y > 0)
