1bf4bab383fb940431d6024a090ff3e006943ba8
peljevic <j.peljevic.17@ucl.ac.uk>
2018-05-16 10:34:53 +0100

    Creating the GitHub Repository
    Just adding the initial code to begin working

---
+++ RC3-Sandbox-master/Unity/Projects/RC3-Framework/Assets/Examples/01 Dendritic Growth/Scripts/Objects/SharedGraph.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Notes
+ */
+
+namespace RC3.Unity.Examples.DendriticGrowth
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [CreateAssetMenu(menuName = "RC3/Examples/DendriticGrowth/SharedGraph")]
+    public class SharedGraph : SharedGraph<VertexObject> { }
+}
---
+++ RC3-Sandbox-master/Unity/Projects/RC3-Framework/Assets/Examples/04 Territorial Agents (WIP)/Scripts/Objects/SharedEdgeGraph.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Notes
+ */
+
+namespace RC3.Unity.Examples.TerritorialAgents
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [CreateAssetMenu(menuName = "RC3/Examples/TerritorialAgents/SharedEdgeGraph")]
+    public class SharedEdgeGraph : SharedEdgeGraph<VertexObject, EdgeObject>
+    {
+    }
+}
